94c834bd5dab0f3acf8a8ad6368675fab4731a9e
Alansmatias <alamm.hhh@gmail.com>
2022-12-01 06:36:51 -0300

    corrigindo query

--- salvarprocesso.php
+++ salvarprocesso.php
@@ -4,12 +4,14 @@
 			$numero_processo = $_POST["numprocesso"];
 			$cpfcnpj = $_POST["cpfcnpj"];
 			$nomeinteressado = $_POST["nomeinteressado"];
-			$dataproc = $_POST["dataproc"];
 			$statusproc = $_POST["statusproc"];
+			$dataproc = $_POST["dataproc"];
 
-			$sql = "INSERT INTO tb_processo (id_processo, nome_interessado, cpfcnpj) VALUES ('{$numero_processo}', '{$nomeinteressado}', '{$cpfcnpj}');". "INSERT INTO tb_statusProcesso (tb_processo_id_processo, status_processo, dataStatus) VALUES ('{$numero_processo}','{$statusproc}');";
+			$sql = "INSERT INTO tb_processo (id_processo, nome_interessado, cpfcnpj) VALUES ('{$numero_processo}', '{$nomeinteressado}', '{$cpfcnpj}');";
+			$sql2 = "INSERT INTO tb_statusProcesso (tb_processo_id_processo, status_processo, dataStatus) VALUES ('{$numero_processo}','{$statusproc}','{$dataproc}');";
 			
 			$res = $conn->query($sql);
+			$res2 = $conn->query($sql2);
 			break;
 		case 'editar':
 			// code...
